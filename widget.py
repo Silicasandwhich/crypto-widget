a184503908b136d21ec560aa39c4a8184aed34ce
SilicaSandwhich <43558271+Silicasandwhich@users.noreply.github.com>
2018-10-26 09:22:30 -0400

    commit to sync changes on iPhone

--- widget.py
+++ widget.py
@@ -1,2 +1,34 @@
 import ui
 import time
+import clipboard
+import sys
+
+key = "test"
+
+
+def encrypt(Text, Key):
+    text = Text.lower()
+    key = Key.lower
+    lent = len(text)
+    lenk = len(key)
+    h = ""
+    
+    if lent > lenk:
+			tk=lent-lenk
+			for x in range(0, (tk)):
+				key = key + key[x]
+			
+			for y in range(0, lent):
+				to1_26text = ord(word[y])-97
+					to1_26key = ord(key[y])-97
+					s = to1_26text + to1_26key
+					ms = s%26
+					c=chr(ms+97)
+					h = h + c
+		
+    
+    
+def encrypt_button_tapped(sender):
+    clipboard.set(encrypt(clipboard.get(), key)
+    sender.title = "encrypted!"
+    
